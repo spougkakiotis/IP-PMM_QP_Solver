f1e18c9fe1b6021a532f6cf5db5a1a81cbcfefd4
spougkakiotis <33895657+spougkakiotis@users.noreply.github.com>
2021-07-14 17:57:36 +0300

    Add files via upload

--- Output_functions/IP_PMM_header.m
+++ Output_functions/IP_PMM_header.m
@@ -1,26 +1,40 @@
-function IP_PMM_header(fid,pl)
+function IP_PMM_header(fid,pl,la_mode)
 % =================================================================================================================== %
 % header function: (for output on a give file, indicated by fid)
 % pl = 1: primal-dual infeasibility and mu is printed at each iteration k
-% pl = 2: primal-dual infeasibility, mu, sigma, and step-lengths are printed at each iteration k
+% pl = 2: primal-dual infeasibility, mu, and step-lengths and penalty parameters are printed at each iteration k
+% la_mode = "exact": no Krylov method employed
+% la_mode = "inexact": also print number of Krylov iterations, nnz of preconditioner, direction error
+% 
 % ------------------------------------------------------------------------------------------------------------------- %
+    if (nargin < 3 || isempty(la_mode)) la_mode = "exact"; end
     if (pl >= 1)
         fprintf(fid,' ');
         fprintf(fid,'%4s    ', 'iter');
         fprintf(fid,'%8s  ',   'pr feas');
         fprintf(fid,'%8s  ',   'dl feas');
-        fprintf(fid,'%8s  ',   'mu');
+        fprintf(fid,'%6s  ',   'mu');
     end
     if (pl >= 2)
         fprintf(fid,'  ');
         fprintf(fid,'%8s  ', 'alpha_x');
         fprintf(fid,'%8s  ', 'alpha_z');
+        fprintf(fid,'%7s  ', 'delta');
+        fprintf(fid,'%7s  ', 'rho');
     end
+    if (la_mode == "inexact")
+        fprintf(fid,'%12s   ', 'Krylov its');
+        fprintf(fid,'%9s  ', 'nnz(Prec)');
+    end
+
     if (pl >= 1)
         fprintf(fid,'\n ====    ========  ========  ========');
     end
     if (pl >= 2)
-        fprintf(fid,'    ========  ========');
+        fprintf(fid,'  ========  ========  ========  ========');
+    end
+    if (la_mode == "inexact")
+        fprintf(fid,'  ===========  =========');
     end
     if (pl >= 1) fprintf(fid,'\n'); end
 % ___________________________________________________________________________________________________________________ %
--- Output_functions/IP_PMM_output.m
+++ Output_functions/IP_PMM_output.m
@@ -1,20 +1,35 @@
-function IP_PMM_output(fid,pl,it,xinf,sinf,mu,alpha_x,alpha_z)
+function IP_PMM_output(fid,pl,it,xinf,sinf,mu,alpha_x,alpha_z,delta,rho,la_mode,Krylov_its,nnz_P)
 % =========================================================== %
 % This function outputs the residual infeasibilities
 % and other statistics of IP-PMM, to a give FID (file
 % indicator).
+% la_mode = "exact": no Krylov method employed
+% la_mode = "inexact": also print number of Krylov iterations, nnz of preconditioner, direction error
 % ----------------------------------------------------------- %
+    if (nargin < 11 || isempty(la_mode))
+        la_mode = "exact";
+        Krylov_its = [];
+        nnz_P = [];
+    elseif (nargin == 12)
+        error('IP_PMM_output: Not enough input arguments.\n');
+    end
     if (pl >= 1)
         fprintf(fid,' ');
-        fprintf(fid,'%4d    ', it);
+        fprintf(fid,'%3d     ', it);
         fprintf(fid,'%8.2e  ', xinf);
         fprintf(fid,'%8.2e  ', sinf);
-        fprintf(fid,'%8.2e  ', mu);
+        fprintf(fid,'%8.2e ', mu);
     end
     if (pl >= 2)
-        fprintf(fid,'  ');
-        fprintf(fid,'%8.2e  ', alpha_x);
+        fprintf(fid,' ');
+        fprintf(fid,'%2.2e  ', alpha_x);
         fprintf(fid,'%8.2e  ', alpha_z);
+        fprintf(fid,'%8.2e  ', delta);
+        fprintf(fid,'%8.2e  ', rho);
+    end
+    if (la_mode == "inexact")
+        fprintf(fid,'    %4d      ', Krylov_its);
+        fprintf(fid,'%8.2e  ', nnz_P);
     end
     if (pl >= 1) fprintf(fid,'\n'); end
 % ___________________________________________________________ %
